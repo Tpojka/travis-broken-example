79f568e23f7a443d4708e72e65504b1f44959779
Goran Grbic <tpojka@gmail.com>
2017-07-24 21:16:23 +0000

    Added PHPUnit

--- Test.php
+++ Test.php
@@ -1,6 +1,10 @@
 <?php
 
-class Test extends PHPUnit_Framework_TestCase
+require __DIR__.'/vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
 {
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
